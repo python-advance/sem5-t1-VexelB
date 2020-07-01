9d66601ce3b288ac58ae753b1707c5a6474afb14
Voytin Evgeny <vexelb@gmail.com>
2020-07-01 19:32:28 +0300

    Update isr.py

--- isr.py
+++ isr.py
@@ -1,16 +1,25 @@
-value = int(input("Введите максимальное число: \t" ))
+class Gost_Kniga():
+    def __init__(self):
+        self.guests = list()
 
-def fib(value):
-  lst = [0, 1, ]
-  it = iter(list(range(2, value)))
-  while True:
-    try:
-      elem = next(it)
-    except StopIteration:
-      break
-    n = lst[elem - 1] + lst[elem - 2]
-    lst.append(n)
-  return lst
 
-res = fib(value)
-print(res)
+    def add(self, first_name, second_name, age): #добавляем человечка с нужными нам параметрами(имя,фамилия,возраст,страна)
+        self.guests.append({"first_namename": first_name,"second_name": second_name, "age": age})
+    
+    def delete(self, first_name):
+        for guests in self.guests:
+            if guests.get("guests_age") == age: 
+                self.guests.remove(guests) 
+
+    def fail(self): 
+        import json
+        with open("Гостевая книга.json", 'a') as file:
+            json_data = { "Guests": self.guests }
+            file.write(json.dumps(json_data, indent=4))
+            
+if __name__ == "__main__":
+    Gost_Kniga = Gost_Kniga()
+    Gost_Kniga.add("Ivan", "Ivanov", 16)
+    Gost_Kniga.add("Vladimir", "Putin", 18)
+    
+    Gost_Kniga.fail()
